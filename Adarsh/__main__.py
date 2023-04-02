6d23dad2279b65f33dd742258951c86df8b7acc2
vasudevareddy187 <127773975+vasudevareddy187@users.noreply.github.com>
2023-04-02 19:59:49 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -34,4 +34,102 @@ async def start_services():
     bot_info = await StreamBot.get_me()
     StreamBot.username = bot_info.username
     print("------------------------------ DONE ------------------------------")
-    
+    print()
+
+    print(
+
+        "---------------------- Initializing Clients ----------------------"
+
+    )
+
+    await initialize_clients()
+
+    print("------------------------------ DONE ------------------------------")
+
+    print('\n')
+
+    print('--------------------------- Importing ---------------------------')
+
+    for name in files:
+
+        with open(name) as a:
+
+            patt = Path(a.name)
+
+            plugin_name = patt.stem.replace(".py", "")
+
+            plugins_dir = Path(f"Adarsh/bot/plugins/{plugin_name}.py")
+
+            import_path = ".plugins.{}".format(plugin_name)
+
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+
+            load = importlib.util.module_from_spec(spec)
+
+            spec.loader.exec_module(load)
+
+            sys.modules["Adarsh.bot.plugins." + plugin_name] = load
+
+            print("Imported => " + plugin_name)
+
+    if Var.ON_HEROKU:
+
+        print("------------------ Starting Keep Alive Service ------------------")
+
+        print()
+
+        asyncio.create_task(ping_server())
+
+    print('-------------------- Initalizing Web Server -------------------------')
+
+    app = web.AppRunner(await web_server())
+
+    await app.setup()
+
+    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADRESS
+
+    await web.TCPSite(app, bind_address, Var.PORT).start()
+
+    print('----------------------------- DONE ---------------------------------------------------------------------')
+
+    print('\n')
+
+    print('---------------------------------------------------------------------------------------------------------')
+
+    print('---------------------------------------------------------------------------------------------------------')
+
+    print(' follow me for more such exciting bots! https://github.com/adarsh-goel')
+
+    print('---------------------------------------------------------------------------------------------------------')
+
+    print('\n')
+
+    print('----------------------- Service Started -----------------------------------------------------------------')
+
+    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
+
+    print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
+
+    print('                        Owner =>> {}'.format((Var.OWNER_USERNAME)))
+
+    if Var.ON_HEROKU:
+
+        print('                        app runnng on =>> {}'.format(Var.FQDN))
+
+    print('---------------------------------------------------------------------------------------------------------')
+
+    print('Give a star to my repo https://github.com/adarsh-goel/filestreambot-pro  also follow me for new bots')
+
+    print('---------------------------------------------------------------------------------------------------------')
+
+    await idle()
+
+if __name__ == '__main__':
+
+    try:
+
+        loop.run_until_complete(start_services())
+
+    except KeyboardInterrupt:
+
+        logging.info('----------------------- Service Stopped -----------------------')
